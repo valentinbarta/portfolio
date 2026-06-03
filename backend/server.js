6dfa1acb6c442160563efeae49c97bdf406a09e0
Valentin <bartavalentin8@gmail.com>
2026-06-02 23:12:56 -0500

    Change fronend url

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000
 // Middleware
 app.use(express.json())
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'https://portfolio-valentinb.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
